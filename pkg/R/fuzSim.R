d3ed9014c9a79cd8d207326ae9589fb6afc9b985
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2014-05-08 15:02:43 +0000

    removed unnecessary parentheses in Baroni
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fuzSim.R
+++ pkg/R/fuzSim.R
@@ -3,14 +3,14 @@ function(x, y, method) {
   stopifnot(length(x) == length(y), 
             x >= 0, x <= 1, 
             y >= 0, y <= 1, 
-            method %in% c('Baroni', 'Jaccard', 'Sorensen'))
+            method %in% c("Jaccard", "Sorensen", "Baroni"))
   A <- sum(x)
   B <- sum(y)
   C <- sum(pmin(x, y))
-  if (method == "Baroni") {
-    D <- sum(1 - pmax(x, y))
-    return((sqrt(C * D) + C) / ((sqrt(C * D)) + A + B - C))
-  }
-  else if (method == "Jaccard") return(C / (A + B - C))
+  if (method == "Jaccard") return(C / (A + B - C))
   else if (method == "Sorensen") return(2 * C / (A + B))
+  else if (method == "Baroni") {
+    D <- sum(1 - pmax(x, y))
+    return((sqrt(C * D) + C) / (sqrt(C * D) + A + B - C))
+  }
 }

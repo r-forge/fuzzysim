164732cfa852fff5c4383edf3b5e5b91598c9106
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2016-03-22 17:55:36 +0000

    fuzzyRangeChange now has measure choice, barplot and negative loss
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fuzzyRangeChange.R
+++ pkg/R/fuzzyRangeChange.R
@@ -1,29 +1,43 @@
-fuzzyRangeChange <- function(pred1, pred2, prop = TRUE, na.rm = TRUE, round.digits = 2) {
+fuzzyRangeChange <- function(pred1, pred2, number = FALSE, prop = TRUE, na.rm = TRUE, round.digits = 2, measures = c("Gain", "Loss", "Stable_presence", "Stable_absence", "Balance"), plot = TRUE, col = colorRampPalette(c("white", "black"))(length(measures)), ...) {
   
-  # version 1.3 (23 Nov 2015)
+  # version 1.4 (22 Mar 2016)
   
   stopifnot(ncol(pred1) == ncol(pred2),
             all(pred1[is.finite(pred1)] >= 0 && pred1[is.finite(pred1)] <= 1),
             all(pred2[is.finite(pred2)] >= 0 && pred2[is.finite(pred2)] <= 1)
   )
   
-  gain <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "expansion", na.rm = na.rm), na.rm = na.rm)
-  loss <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "contraction", na.rm = na.rm), na.rm = na.rm)
-  stable.pres <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "maintenance", na.rm = na.rm), na.rm = na.rm)
-  stable.abs <- sum(fuzzyOverlay(1 - data.frame(pred1, pred2), op = "maintenance", na.rm = na.rm), na.rm = na.rm)
-  balance <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "change", na.rm = na.rm), na.rm = na.rm)
+  values <- vector("numeric", length(measures))
+  names(values) <- measures
+  if ("Gain" %in% measures)  values["Gain"] <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "expansion", na.rm = na.rm), na.rm = na.rm)
+  if ("Loss" %in% measures)  values["Loss"] <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "contraction", na.rm = na.rm), na.rm = na.rm)
+  if ("Stable_presence" %in% measures)  values["Stable_presence"] <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "maintenance", na.rm = na.rm), na.rm = na.rm)
+  if ("Stable_absence" %in% measures)  values["Stable_absence"] <- sum(fuzzyOverlay(1 - data.frame(pred1, pred2), op = "maintenance", na.rm = na.rm), na.rm = na.rm)
+  if ("Balance" %in% measures)  values["Balance"] <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "change", na.rm = na.rm), na.rm = na.rm)
   
-  result <- data.frame(Measure = c("Gain", "Loss", "Stable_presence", "Stable_absence", "Balance"), Number = c(gain, abs(loss), stable.pres, stable.abs, balance))
+  result <- data.frame(Measure = measures, Number = values)
+  #rownames(result) <- NULL
   
   if (prop) {
     if (na.rm) n <- length(na.omit(pred1))
     else n <- length(pred1)
     range.size <- sum(pred1, na.rm = na.rm)
     stable.abs <- result[result$Measure == "Stable_absence", "Number"]
-    result[ , "Number"] <- result[ , "Number"] / range.size
-    result[result$Measure == "Stable_absence", "Number"] <- stable.abs / (n - range.size)
-    names(result)[2] <- "Proportion"
+    result$Proportion <- result[ , "Number"] / range.size
+    result[result$Measure == "Stable_absence", "Proportion"] <- stable.abs / (n - range.size)
   }
+  
+  if (!number) {
+    result <- result[ , -1]
+  }
+  
+  if (plot) {
+    barplot(result[ , ncol(result)], legend.text = rownames(result), col = col, ...)
+    abline(h = 0)
+  }
+  
+  result[,1] <- NULL
+  if (is.finite(round.digits))  result <- round(result, round.digits)
 
   result
 }

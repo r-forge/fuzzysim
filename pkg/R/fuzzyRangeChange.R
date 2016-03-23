cf6459b8fdebca1c82eb071dde4113055c24085a
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2016-03-23 10:16:35 +0000

    fuzzyRangeChange now has measure choice, barplot and negative loss
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/fuzzyRangeChange.R
+++ pkg/R/fuzzyRangeChange.R
@@ -1,11 +1,13 @@
-fuzzyRangeChange <- function(pred1, pred2, number = FALSE, prop = TRUE, na.rm = TRUE, round.digits = 2, measures = c("Gain", "Loss", "Stable_presence", "Stable_absence", "Balance"), plot = TRUE, col = colorRampPalette(c("white", "black"))(length(measures)), ...) {
+fuzzyRangeChange <- function(pred1, pred2, number = TRUE, prop = TRUE, na.rm = TRUE, round.digits = 2, measures = c("Gain", "Loss", "Stable_presence", "Stable_absence", "Balance"), plot = TRUE, col = colorRampPalette(c("white", "black"))(length(measures)), ...) {
   
-  # version 1.4 (22 Mar 2016)
+  # version 1.4 (23 Mar 2016)
   
   stopifnot(ncol(pred1) == ncol(pred2),
             all(pred1[is.finite(pred1)] >= 0 && pred1[is.finite(pred1)] <= 1),
             all(pred2[is.finite(pred2)] >= 0 && pred2[is.finite(pred2)] <= 1)
   )
+
+  if (!number & !prop) stop ("Nothing to calculate if both 'number' and 'prop' are FALSE.")
   
   values <- vector("numeric", length(measures))
   names(values) <- measures
@@ -16,7 +18,6 @@ fuzzyRangeChange <- function(pred1, pred2, number = FALSE, prop = TRUE, na.rm = 
   if ("Balance" %in% measures)  values["Balance"] <- sum(fuzzyOverlay(data.frame(pred1, pred2), op = "change", na.rm = na.rm), na.rm = na.rm)
   
   result <- data.frame(Measure = measures, Number = values)
-  #rownames(result) <- NULL
   
   if (prop) {
     if (na.rm) n <- length(na.omit(pred1))
@@ -28,7 +29,7 @@ fuzzyRangeChange <- function(pred1, pred2, number = FALSE, prop = TRUE, na.rm = 
   }
   
   if (!number) {
-    result <- result[ , -1]
+    result <- result[ , - (ncol(result) - 1)]
   }
   
   if (plot) {
@@ -36,7 +37,7 @@ fuzzyRangeChange <- function(pred1, pred2, number = FALSE, prop = TRUE, na.rm = 
     abline(h = 0)
   }
   
-  result[,1] <- NULL
+  result[ , "Measure"] <- NULL
   if (is.finite(round.digits))  result <- round(result, round.digits)
 
   result

42a320196d4e1f6c835944c3e633d6ece2ff249a
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2019-03-10 20:27:09 +0000

    v2.2 with new function sharedFav
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@111 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bioThreat.R
+++ pkg/R/bioThreat.R
@@ -5,15 +5,12 @@ bioThreat <- function(strong_F, weak_F, character = FALSE, ...) {
   weak_F <- favClass(weak_F, ...)
   
   threat <- rep(NA, length(strong_F))
-  for (f in 1:length(strong_F)) {
-    if (is.na(strong_F[f]) | is.na(weak_F[f])) next
-    else if (strong_F[f] == 1 | weak_F[f] == 1) threat[f] <- 0
-    else if (strong_F[f] == 3 & weak_F[f] == 3) threat[f] <- 1
-    else if (strong_F[f] <= 2 & weak_F[f] == 3) threat[f] <- 2
-    else if (strong_F[f] == 2 & weak_F[f] == 2) threat[f] <- 3
-    else if (strong_F[f] == 3 & weak_F[f] <= 2) threat[f] <- 4
-  }  # end for f
-  
+  threat[strong_F == 1 | weak_F == 1] <- 0
+  threat[strong_F == 3 & weak_F == 3] <- 1
+  threat[strong_F <= 2 & weak_F == 3] <- 2
+  threat[strong_F == 2 & weak_F == 2] <- 3
+  threat[strong_F == 3 & weak_F <= 2] <- 4
+
   if (character) {
     threat[!is.na(threat) & threat == 0] <- "white"
     threat[!is.na(threat) & threat == 1] <- "green"
@@ -21,7 +18,6 @@ bioThreat <- function(strong_F, weak_F, character = FALSE, ...) {
     threat[!is.na(threat) & threat == 3] <- "orange"
     threat[!is.na(threat) & threat == 4] <- "red"
   }
-  
   else threat <- as.integer(threat)
   
   return(threat)

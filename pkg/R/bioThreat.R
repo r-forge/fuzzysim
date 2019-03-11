a52ec8386f28d92fa5730195079998e113ef546e
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2019-03-11 08:35:50 +0000

    fixed bug in bioThreat
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bioThreat.R
+++ pkg/R/bioThreat.R
@@ -5,18 +5,18 @@ bioThreat <- function(strong_F, weak_F, character = FALSE, ...) {
   weak_F <- favClass(weak_F, ...)
   
   threat <- rep(NA, length(strong_F))
-  threat[strong_F == 1 | weak_F == 1] <- 0
-  threat[strong_F == 3 & weak_F == 3] <- 1
-  threat[strong_F <= 2 & weak_F == 3] <- 2
-  threat[strong_F == 2 & weak_F == 2] <- 3
   threat[strong_F == 3 & weak_F <= 2] <- 4
+  threat[strong_F == 2 & weak_F == 2] <- 3
+  threat[strong_F <= 2 & weak_F == 3] <- 2
+  threat[strong_F == 3 & weak_F == 3] <- 1
+  threat[strong_F == 1 | weak_F == 1] <- 0
 
   if (character) {
-    threat[!is.na(threat) & threat == 0] <- "white"
-    threat[!is.na(threat) & threat == 1] <- "green"
-    threat[!is.na(threat) & threat == 2] <- "yellow"
-    threat[!is.na(threat) & threat == 3] <- "orange"
     threat[!is.na(threat) & threat == 4] <- "red"
+    threat[!is.na(threat) & threat == 3] <- "orange"
+    threat[!is.na(threat) & threat == 2] <- "yellow"
+    threat[!is.na(threat) & threat == 1] <- "green"
+    threat[!is.na(threat) & threat == 0] <- "white"
   }
   else threat <- as.integer(threat)
   

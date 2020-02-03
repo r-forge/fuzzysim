f43fbebe34f156f89a9cc881d39f63de218049c3
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2020-02-03 19:24:05 +0000

    fixed gridRecords when no gridded absences and added na.rm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gridRecords.R
+++ pkg/R/gridRecords.R
@@ -1,10 +1,15 @@
 gridRecords <- function(rst,
                         pres.coords,
-                        abs.coords = NULL) {
+                        abs.coords = NULL,
+                        na.rm = TRUE) {
+  
+  # version 2.0 (3 Feb 2020)
   
   if (!requireNamespace("raster")) stop("This function requires installing the 'raster' package first.")
   
-  if (is.null(abs.coords)) abs.coords <- raster::coordinates(rst)
+  if (is.null(abs.coords)) {
+    abs.coords <- raster::coordinates(rst)
+  }
 
   p_extract <- raster::extract(rst, pres.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
   a_extract <- raster::extract(rst, abs.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
@@ -18,7 +23,18 @@ gridRecords <- function(rst,
   a_centroids <- raster::xyFromCell(rst, a_extract$cells)
   
   p_extract <- data.frame(presence = 1, p_centroids, p_extract)
-  a_extract <- data.frame(presence = 0, a_centroids, a_extract)
+  if (nrow(a_extract) > 0) {
+    a_extract <- data.frame(presence = 0, a_centroids, a_extract)
+  }
   
   result <- rbind(p_extract, a_extract)
+  
+  if (na.rm) {
+    result_NA <- which(apply(result[ , 5:ncol(result)], MARGIN = 1, FUN = function(x) all(is.na(x))))
+    if (length(result_NA) > 0) {
+      result <- result[-result_NA, ]
+    }
+  }
+  
+  return(result)
 }

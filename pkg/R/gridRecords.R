de2a7f437cd0ab8c0ff53be8aaa2e03dda61ec08
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2020-09-10 16:39:14 +0000

    added 'absences' argument to 'gridRecords'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gridRecords.R
+++ pkg/R/gridRecords.R
@@ -1,40 +1,41 @@
 gridRecords <- function(rst,
                         pres.coords,
                         abs.coords = NULL,
+                        absences = TRUE,
                         na.rm = TRUE) {
-  
-  # version 2.0 (3 Feb 2020)
-  
+
+  # version 2.1 (10 Sep 2020)
+
   if (!requireNamespace("raster")) stop("This function requires installing the 'raster' package first.")
-  
-  if (is.null(abs.coords)) {
-    abs.coords <- raster::coordinates(rst)
-  }
 
   p_extract <- raster::extract(rst, pres.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
-  a_extract <- raster::extract(rst, abs.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
-  
   p_extract <- unique(p_extract)
-  a_extract <- unique(a_extract)
-  
-  a_extract <- a_extract[!(a_extract$cells %in% p_extract$cells), ]
-  
   p_centroids <- raster::xyFromCell(rst, p_extract$cells)
-  a_centroids <- raster::xyFromCell(rst, a_extract$cells)
-  
   p_extract <- data.frame(presence = 1, p_centroids, p_extract)
-  if (nrow(a_extract) > 0) {
-    a_extract <- data.frame(presence = 0, a_centroids, a_extract)
+
+  if (absences == TRUE) {
+    if (is.null(abs.coords)) {
+      abs.coords <- raster::coordinates(rst)
+    }
+    a_extract <- raster::extract(rst, abs.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
+    a_extract <- unique(a_extract)
+    a_extract <- a_extract[!(a_extract$cells %in% p_extract$cells), ]
+    a_centroids <- raster::xyFromCell(rst, a_extract$cells)
+    if (nrow(a_extract) > 0) {
+      a_extract <- data.frame(presence = 0, a_centroids, a_extract)
+    }
+  } else {
+    a_extract <- NULL
   }
-  
+
   result <- rbind(p_extract, a_extract)
-  
+
   if (na.rm) {
     result_NA <- which(apply(result[ , 5:ncol(result)], MARGIN = 1, FUN = function(x) all(is.na(x))))
     if (length(result_NA) > 0) {
       result <- result[-result_NA, ]
     }
   }
-  
+
   return(result)
 }

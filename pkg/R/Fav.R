d543360ce24fc34b635188e9a42cb06805c7ec47
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2021-10-03 21:28:41 +0000

    Fav takes gam, gbm, rf, bart; Fav and gridRecords take SpatRaster
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Fav.R
+++ pkg/R/Fav.R
@@ -1,66 +1,68 @@
 Fav <- function(model = NULL, obs = NULL, pred = NULL, n1n0 = NULL, sample.preval = NULL, method = "RBV", true.preval = NULL, verbosity = 2) {
-    # version 1.4 (24 Apr 2021)
+  # version 1.5 (30 Sep 2021)
+  
+  if (!is.null(model)) {
+    if (verbosity > 0) {
+      if (!is.null(pred)) message("Argument 'pred' ignored in favour of 'model'.")
+      if (!is.null(obs)) message("Argument 'obs' ignored in favour of 'model'.")
+      if (!is.null(n1n0)) message("Argument 'n1n0' ignored in favour of 'model'.")
+      if (!is.null(sample.preval)) message("Argument 'sample.preval' ignored in favour of 'model'.")
+    }  # end if verbosity
     
-    if (!is.null(model)) {
-      if (verbosity > 0) {
-        if (!is.null(pred)) message("Argument 'pred' ignored in favour of 'model'.")
-        if (!is.null(obs)) message("Argument 'obs' ignored in favour of 'model'.")
-        if (!is.null(n1n0)) message("Argument 'n1n0' ignored in favour of 'model'.")
-        if (!is.null(sample.preval)) message("Argument 'sample.preval' ignored in favour of 'model'.")
-      }
+    if (is(model, "glm") || is(model, "gam")) {
       obs <- model$y
       pred <- model$fitted.values
-    }  # end if model
-    
-    # if(!is.null(obs) & !is.null(pred) & !class(pred) == "RasterLayer" & length(obs) != length(pred)) {
-    #   stop("'obs' and 'pred' must have the same length (and be in the same order).")
-    # }
-    
-    if (!is.null(obs)) {
-      n1 <- sum(obs == 1, na.rm = TRUE)
-      n0 <- sum(obs == 0, na.rm = TRUE)
-    }  # end if obs
-    
-    else if (!is.null(n1n0)) {
-      if (verbosity > 0) {
-        if(!is.null(model)) message("Argument 'n1n0' ignored in favour of 'model'")
-        else if (!is.null(obs)) message("Argument 'n1n0' ignored in favour of 'obs'")
-      }
-      n1 <- n1n0[1]
-      n0 <- n1n0[2]
-      #if(n1 + n0 != length(pred)) stop("n1+n0 must equal the length of 'pred'.")
-    }  # end if n1n0
-    
-    else if (!is.null(sample.preval)) {
-      if (verbosity > 0) {
-        if(!is.null(model)) message("Argument 'sample.preval' ignored
-                                in favour of 'model'")
-        else if (!is.null(obs)) message("Argument 'sample.preval' ignored
-                                    in favour of 'obs'")
-        else if (!is.null(n1n0)) message("Argument 'sample.preval' ignored
-                                     in favour of 'n1n0'")
-      }
-      n1 <- sample.preval * 100
-      n0 <- 100 - n1
-    }  # end if sample.preval
-    
-    else stop("You need to provide either 'model', or 'pred' plus either one of 'obs', 'n1n0' or 'sample.preval'.")
-    
-    # slightly reduce probabilities of exactly 1, which would cause division by zero:
-    # (resulting favourability is still 1)
-    pred[pred == 1] <- 1 - 2.2e-16
-    
-    if(method == "RBV") {  # Real, Barbosa & Vargas 2006
-      fav <- (pred / (1 - pred)) / ((n1 / n0) + (pred / (1 - pred)))
+    } else if (is(model, "gbm")) {
+      obs <- model$data$y
+      exp_y <- exp(model$fit)  # gbm provides 'fit' on the predictors' scale
+      pred <- exp_y / (1 + exp_y)  # logit to probability
+    } else if (is(model, "randomForest")) {
+      obs <- as.integer(as.character(model$y))
+      pred <- predict(model, type = "prob")[ , "1"]
+    } else if (is(model, "bart")) {
+      if (is.null(model$fit$data)) stop("'$fit$data' section not available in 'model'. Try computing the 'bart' model with 'keeptrees=TRUE', or providing other arguments here instead of 'model', i.e. 'pred' plus one of 'obs', 'n1n0' or 'sample.preval'.")
+      obs <- model$fit$data@y  # requires model ran with keeptrees=TRUE
+      pred <- stats::fitted(model, type = "response")
+    } else stop("'model' is of a non-implemented class.")
+  }  # end if model
+  
+  # if(!is.null(obs) & !is.null(pred) & !class(pred) == "RasterLayer" & length(obs) != length(pred)) {
+  #   stop("'obs' and 'pred' must have the same length (and be in the same order).")
+  # }
+  
+  vals <- na.omit(obs)
+  if (is(model, "randomForest"))  vals <- as.integer(as.character(vals))
+  if (!all(vals %in% c(0, 1))) stop("Favourability is only applicable when the response variable is binary, taking only values of 0 or 1.")
+  
+  if (!is.null(obs)) {
+    n1 <- sum(obs == 1, na.rm = TRUE)
+    n0 <- sum(obs == 0, na.rm = TRUE)
+  }  else if (!is.null(n1n0)) {
+    if (verbosity > 0) {
+      if (!is.null(obs)) message("Argument 'n1n0' ignored in favour of 'obs'")
     }
-    
-    else if(method == "AT") {  # Albert & Thuiller (2008); but see Acevedo & Real (2012)!
-      sample.preval <- n1 / (n1 + n0)
-      fav <- (pred / (1 - pred)) /
-        ((sample.preval / true.preval) + (pred / (1 - pred)))
+    n1 <- n1n0[1]
+    n0 <- n1n0[2]
+    #if(n1 + n0 != length(pred)) stop("n1+n0 must equal the length of 'pred'.")
+  } else if (!is.null(sample.preval)) {
+    if (verbosity > 0) {
+      if (!is.null(obs)) message("Argument 'sample.preval' ignored in favour of 'obs'")
+      else if (!is.null(n1n0)) message("Argument 'sample.preval' ignored in favour of 'n1n0'")
     }
-    
-    else stop("method must be either 'RBV' or 'AT'")
-    
-    return(fav)
-  }
+    n1 <- sample.preval * 100
+    n0 <- 100 - n1
+  } else stop("You need to provide either 'model', or 'pred' plus either one of 'obs', 'n1n0' or 'sample.preval'.")
+  
+  # slightly reduce probabilities of exactly 1, which would cause division by zero (resulting favourability is still 1):
+  pred[pred == 1] <- 1 - 2.2e-16
+  
+  if (method == "RBV") {  # Real, Barbosa & Vargas 2006
+    fav <- (pred / (1 - pred)) / ((n1 / n0) + (pred / (1 - pred)))
+  } else if (method == "AT") {  # Albert & Thuiller (2008); but see Acevedo & Real (2012)!
+    sample.preval <- n1 / (n1 + n0)
+    fav <- (pred / (1 - pred)) /
+      ((sample.preval / true.preval) + (pred / (1 - pred)))
+  } else stop("method must be either 'RBV' or 'AT'")
+  
+  return(fav)
+}
--- pkg/R/gridRecords.R
+++ pkg/R/gridRecords.R
@@ -3,33 +3,63 @@ gridRecords <- function(rst,
                         abs.coords = NULL,
                         absences = TRUE,
                         na.rm = TRUE) {
-
+  
   # version 2.1 (10 Sep 2020)
-
-  if (!requireNamespace("raster")) stop("This function requires installing the 'raster' package first.")
-
-  p_extract <- raster::extract(rst, pres.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
-  p_extract <- unique(p_extract)
-  p_centroids <- raster::xyFromCell(rst, p_extract$cells)
-  p_extract <- data.frame(presence = 1, p_centroids, p_extract)
-
-  if (absences == TRUE) {
-    if (is.null(abs.coords)) {
-      abs.coords <- raster::coordinates(rst)
+  
+  if (!requireNamespace("raster") && !requireNamespace("terra")) stop("This function requires having either the 'raster' or the 'terra' package installed.")
+  
+  if (inherits(rst, "Raster")) {
+    if (requireNamespace("terra"))  rst <- terra::rast(rst)  # for faster processing
+    else {
+      p_extract <- raster::extract(rst, pres.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
+      p_extract <- unique(p_extract)
+      p_centroids <- raster::xyFromCell(rst, p_extract$cells)
+      p_extract <- data.frame(presence = 1, p_centroids, p_extract)
+      
+      if (absences == TRUE) {
+        if (is.null(abs.coords)) {
+          abs.coords <- raster::coordinates(rst)
+        }  # presence coordinates removed below
+        a_extract <- raster::extract(rst, abs.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
+        a_extract <- unique(a_extract)
+        a_extract <- a_extract[!(a_extract$cells %in% p_extract$cells), ]
+        a_centroids <- raster::xyFromCell(rst, a_extract$cells)
+        if (nrow(a_extract) > 0) {
+          a_extract <- data.frame(presence = 0, a_centroids, a_extract)
+        }
+      } else {
+        a_extract <- NULL
+      }
+    }  # end if !terra
+  }  # end if Raster*
+  
+  else if (inherits(rst, "SpatRaster")) {
+    p_extract <- terra::extract(rst, pres.coords, cells = TRUE, xy = FALSE)
+    p_extract$ID <- NULL
+    p_extract <- unique(p_extract)
+    p_centroids <- terra::xyFromCell(rst, p_extract$cell)
+    p_extract <- data.frame(presence = 1, p_centroids, p_extract[ , "cell", drop = FALSE], p_extract[ , 1:terra::nlyr(rst)])
+    
+    if (absences == TRUE) {
+      if (is.null(abs.coords)) {
+        #abs.coords <- coordinates(rst)
+        abs.coords <- terra::geom(terra::centroids(terra::as.points(rst)))[ , c("x", "y")]
+        #abs.coords <- terra::xyFromCell(rst, cells(rst))
+      }  # presence coordinates removed below
+      a_extract <- terra::extract(rst, abs.coords, cells = TRUE, xy = TRUE)
+      a_extract$ID <- NULL
+      a_extract <- unique(a_extract)
+      a_extract <- a_extract[!(a_extract$cell %in% p_extract$cell), ]
+      if (nrow(a_extract) > 0) {
+        a_extract <- data.frame(presence = 0, a_extract[ , c("x", "y", "cell")], a_extract[ , 1:terra::nlyr(rst)])
+      }
+    } else {
+      a_extract <- NULL
     }
-    a_extract <- raster::extract(rst, abs.coords, cellnumbers = TRUE, df = TRUE)[ , -1]
-    a_extract <- unique(a_extract)
-    a_extract <- a_extract[!(a_extract$cells %in% p_extract$cells), ]
-    a_centroids <- raster::xyFromCell(rst, a_extract$cells)
-    if (nrow(a_extract) > 0) {
-      a_extract <- data.frame(presence = 0, a_centroids, a_extract)
-    }
-  } else {
-    a_extract <- NULL
-  }
-
+  }  # end if SpatRaster
+  
   result <- rbind(p_extract, a_extract)
-
+  
   if (na.rm) {
     result_NA <- which(apply(result[ , 5:ncol(result), drop = FALSE], MARGIN = 1, FUN = function(x) all(is.na(x))))
     if (length(result_NA) > 0) {
@@ -37,5 +67,7 @@ gridRecords <- function(rst,
     }
   }
 
+  colnames(result) <- sub("cells", "cell", colnames(result))  # for uniformity between 'raster' and 'terra' output
+  
   return(result)
 }

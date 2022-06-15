849d32b40f41b7725602a599c98cf7d51774717a
marcia (A. Marcia Barbosa) <ana.marcia.barbosa@gmail.com>
2022-06-15 19:34:46 +0000

    'stepByStep' new arguments 'k', 'direction'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzysim@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stepByStep.R
+++ pkg/R/stepByStep.R
@@ -3,10 +3,12 @@ stepByStep <- function(data,
                        var.cols, 
                        family = binomial(link = "logit"), 
                        Favourability = FALSE, 
-                       trace = 0, 
+                       trace = 0,
+                       direction = "forward",  # argument implemented by Alba Estrada; "forward" default for back-compatibility
+                       k = 2, 
                        cor.method = "pearson") {
   
-  # version 1.5 (14 Jun 2022)
+  # version 1.6 (15 Jun 2022)
   
   data <- as.data.frame(data)
   
@@ -20,15 +22,17 @@ stepByStep <- function(data,
   
   null.model.formula <- as.formula(paste(response, "~", 1))
   scope.formula <- as.formula(paste("~", paste(vars, collapse = "+")))
-  mod <- step(glm(null.model.formula, family = family, data = data), scope = scope.formula, direction = "forward", trace = trace)
+  mod <- step(glm(null.model.formula, family = family, data = data), scope = scope.formula, direction = direction, trace = trace, k = k)
   pred.final <- mod$fitted.values
   
   if (!all(c("binomial", "logit") %in% mod$family))  Favourability <- FALSE
   if (Favourability)  fav.final <- Fav(model = mod)
   
-  model.vars.split <- sapply(mod$anova[ , 1], strsplit, split = " ")
-  model.vars <- lapply(model.vars.split, `[`, 2)
-  model.vars <- as.character(model.vars)[-1]
+  # model.vars.split <- sapply(mod$anova[ , 1], strsplit, split = " ")
+  # model.vars <- lapply(model.vars.split, `[`, 2)
+  # model.vars <- as.character(model.vars)[-1]
+  model.vars <- mod$anova[ , 1][-1]  # changed by Alba Estrada to accommodate direction="both"
+
   n.steps <- length(model.vars)
   
   preds <- favs <- as.data.frame(matrix(nrow = nrow(data), ncol = n.steps))
@@ -37,7 +41,8 @@ stepByStep <- function(data,
   
   for (s in 1:n.steps) {
     step.vars <- model.vars[1:s]
-    mod.step <- glm(as.formula(paste(response, "~", paste(step.vars, collapse = "+"))), family = family, data = data)
+    #mod.step <- glm(as.formula(paste(response, "~", paste(step.vars, collapse = "+"))), family = family, data = data)
+    mod.step <- glm(as.formula(paste(response, "~", paste(step.vars, collapse = ""))), family = family, data = data)  # changed by Alba Estrada to accommodate direction="both"
     if (Favourability) {
       favs[ , s] <- Fav(model = mod.step)
       cor.F[s] <- cor(favs[ , s], fav.final)
